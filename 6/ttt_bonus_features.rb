907492932d0404966bbabfa6914495e579954fe6
lapis-lark <lapis.santuri@gmail.com>
2021-05-19 11:51:37 +0900

    refactoring ttt complete!

--- 6/ttt_bonus_features.rb
+++ 6/ttt_bonus_features.rb
@@ -9,6 +9,9 @@ WINNING_COMBOS = [
 
 PLAYER = 'X'
 CPU = 'O'
+SCORE = { PLAYER => 0, CPU => 0 }
+MAX_WINS = 5
+WIN_NUM = 3
 
 VICTORY = <<-'MSG'
 ___________________________________________________________________________________________
@@ -35,16 +38,22 @@ DEFEAT = <<-'MSG'
 MSG
 
 def display_board
-  puts " #{BOARD[1]} | #{BOARD[2]} | #{BOARD[3]}"
-  puts '---+---+---'
-  puts " #{BOARD[4]} | #{BOARD[5]} | #{BOARD[6]}"
-  puts '---+---+---'
-  puts " #{BOARD[7]} | #{BOARD[8]} | #{BOARD[9]}"
+  board = BOARD.map { |k, v| BOARD[k] == ' ' ? k : v }
+  printable = []
+  board.each_slice(3) do |row|
+    printable << " #{row[0]} | #{row[1]} | #{row[2]}\n"
+  end
+
+  puts printable.join("---+---+---\n")
   puts ''
 end
 
 def clear_screen
-  system('clear') || system('cls')
+  system('clear')
+end
+
+def prompt(str)
+  puts "~~> #{str}\n\n"
 end
 
 def joinor(arr, sym = ', ', conj = 'or')
@@ -83,31 +92,23 @@ def player_turn
   end
 end
 
-def cpu_defense
+def critical_move(type)
+  defense = [CPU, PLAYER]
+  defense.reverse! if type == 'offense'
   WINNING_COMBOS.each do |combo|
     values = BOARD.values_at(*combo)
-    next if values.include?(CPU)
-    next unless values.count(PLAYER) == 2
-    return combo[values.index(' ')]
-  end
-  false
-end
-
-def cpu_offense
-  WINNING_COMBOS.each do |combo|
-    values = BOARD.values_at(*combo)
-    next if values.include?(PLAYER)
-    next unless values.count(CPU) == 2
+    next if values.include?(defense[0])
+    next unless values.count(defense[1]) == WIN_NUM - 1
     return combo[values.index(' ')]
   end
   false
 end
 
 def computer_turn
-  square = if cpu_offense
-             cpu_offense
-           elsif cpu_defense
-             cpu_defense
+  square = if critical_move('offense')
+             critical_move('offense')
+           elsif critical_move('defense')
+             critical_move('defense')
            elsif BOARD[5] == ' '
              5
            else
@@ -121,16 +122,16 @@ def tie?
   BOARD.values.none? { |v| v == ' ' }
 end
 
-def update_score(score, round_winner)
-  score[round_winner] += 1
+def update_score(round_winner)
+  SCORE[round_winner] += 1
 end
 
 def winner?
   WINNING_COMBOS.each do |combo|
     group = BOARD.values_at(*combo)
-    if group.count(PLAYER) == 3
+    if group.count(PLAYER) == WIN_NUM
       return PLAYER
-    elsif group.count(CPU) == 3
+    elsif group.count(CPU) == WIN_NUM
       return CPU
     end
   end
@@ -143,17 +144,27 @@ def display_winner(winner)
   winner
 end
 
-def prompt(str)
-  puts "~~> #{str}\n\n"
+def valid_yes?
+  loop do
+    ans = gets.chomp
+    case ans
+    when /\b(y|yes)\b/i
+      break true
+    when /\b(n|no)\b/i
+      break false
+    else
+      prompt('please input either y/yes or n/no')
+    end
+  end
 end
 
 def change_player_character
   prompt('is "X" okay as your marker?')
-  unless gets.chomp =~ /(y|yes)/i
+  unless valid_yes?
     loop do
-      prompt("input a single character other than 'O' ")
+      prompt('input a single, non-integer character that\'s not a circle')
       char = gets.chomp
-      if char.size == 1 && char != 'O'
+      if char =~ /\b[^O\d\s]\b/
         PLAYER[0] = char
         break
       else
@@ -163,57 +174,79 @@ def change_player_character
   end
 end
 
-def display_score(score)
-  puts "YOU: #{score[PLAYER]}"
-  puts "CPU: #{score[CPU]}"
+def display_score
+  puts "YOU: #{SCORE[PLAYER]}"
+  puts "CPU: #{SCORE[CPU]}"
   puts ""
 end
 
-def display_grand_winner(score)
+def display_grand_winner
   clear_screen
-  display_score(score)
-  score[PLAYER] == 5 ? (puts VICTORY) : (puts DEFEAT)
+  display_score
+  SCORE[PLAYER] == MAX_WINS ? (puts VICTORY) : (puts DEFEAT)
 end
 
-def grand_winner?(score)
-  score.value?(5)
+def grand_winner?
+  SCORE.value?(MAX_WINS)
+end
+
+def reset
+  BOARD.each { |k, _| BOARD[k] = ' ' }
 end
 
 def play_again
   prompt("play again?")
+  if valid_yes?
+    reset
+    true
+  else
+    false
+  end
+end
+
+def display_welcome_message
+  clear_screen
+  prompt("let's play TIC TAC TOE!!!")
+  prompt('earn eternal glory by reaching five victories before the AI')
+end
+
+def turn_type
+  prompt("do you want to go first each round, " \
+  "last each round, or have it be random?")
+  prompt('input "f" for first, "l" for last, or "r" for random')
   loop do
     ans = gets.chomp
-    case ans
-    when /(y|yes)/i
-      BOARD.each { |k, _| BOARD[k] = ' ' }
-      break true
-    when /(n|no)/i
-      break false
-    else
-      prompt('please input either y/yes or n/no')
-    end
+    break ans if ans =~ /\b(f|l|r)\b/
+    prompt("invalid input. plz try again.")
+  end
+end
+
+def decide_first_player(type)
+  case type
+  when 'f' then true
+  when 'l' then false
+  when 'r' then [true, false].sample
   end
 end
 
 # body
-prompt("let's play TIC TAC TOE!!!")
-prompt('earn eternal glory by reaching five victories before the AI')
+display_welcome_message
 change_player_character
-score = { PLAYER => 0, CPU => 0 }
+type = turn_type
 
 loop do
-  turn = [true, false].sample
+  turn = decide_first_player(type)
   loop do
     clear_screen
-    display_score(score)
+    display_score
     turn ? player_turn : computer_turn
     turn = !turn
 
     round_winner = winner?
     if round_winner
-      update_score(score, round_winner)
+      update_score(round_winner)
       clear_screen
-      display_score(score)
+      display_score
       display_board
       display_winner(round_winner)
       break
@@ -223,8 +256,8 @@ loop do
     end
   end
 
-  if grand_winner?(score)
-    display_grand_winner(score)
+  if grand_winner?
+    display_grand_winner
     break
   end
 
