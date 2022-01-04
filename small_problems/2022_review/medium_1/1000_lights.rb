c752d10901ea9beee4a0a57c42e18c78dccad5be
lapis-lark <lapis.santuri@gmail.com>
2022-01-05 07:48:28 +0900

    revised 1000 lights

---
+++ small_problems/2022_review/medium_1/1000_lights.rb
@@ -0,0 +1,13 @@
+def how_many_lights(lights)
+  l_arr = []
+
+  1.upto(lights) do |round|
+    (round..lights).step(round) do |l|
+      l_arr.include?(l) ? l_arr.delete(l) : l_arr << l
+    end
+  end
+  
+  l_arr
+end
+
+p how_many_lights(5)
