6a7835fcb8acd5572c4c0e910f4f2a8ef8f19c7b
lapis-lark <lapis.santuri@gmail.com>
2022-03-24 15:25:01 -0600

    RB109 study prep

--- 109_assessment/common_phrase.rb
+++ 109_assessment/common_phrase.rb
@@ -1,3 +1,10 @@
 a = 'poodle'
 # local variable a is assigned to the String 'poodle'
 
+repeater('Hello')
+# #repeater is invoked on line 4 with the String 'Hello' passed in as an argument
+
+a = 'poodle'
+a = 'labrador'
+# local variable a is assigned to the String 'poodle'
+# local variable a is reassigned to reference the String 'labrador'
--- 109_assessment/public_practice_space/master_study_list.rb
+++ 109_assessment/public_practice_space/master_study_list.rb
@@ -47,14 +47,76 @@ end
 
 =end
 
+# Lesson 2 Section 18, Variable Scope
+# methods can access objects passed but have a totally separate scope
+def some_method(a)
+  puts a
+  puts y
+end
+
+x = 1
+y = 2
+
+some_method(x) # error thrown
+=begin
+
+=end
+
+
 # MUTATING VS NON-MUTATING METHODS
 
+# What is the difference between a mutating and non-mutating method?
+=begin
 
+=end
+
+# What is the value of 's'? Of 't'? Do 's' and 't' reference the same object?
+def fix(value)
+  value.upcase!
+  value.concat('!')
+  value
+ends = 'hello'
+t = fix(s)
+=begin
+
+=end
 
 # PASS-BY-REFERENCE/PASS-BY-VALUE
+# Lesson 2 Section 20
+# Ruby is essentially pass-by-reference, but because not all objects are mutable (i.e. integers, symbosl) and not all methods mutate the caller, it can sometimes behave like pass-by-value
+def add_name(arr, name)
+  arr = arr + [name]
+end
 
+names = ['bob', 'kim']
+add_name(names, 'jim')
+puts names.inspect          # => ["bob", "kim"]
+=begin
 
-# WORKING WITH COLLECTIONS (ARRAY, HASH, STRING); 
+=end
+
+def add_name(arr, name)
+  arr << name
+end
+
+names = ['bob', 'kim']
+add_name(names, 'jim')
+puts names.inspect          # => ["bob", "kim", "jim"]
+=begin
+
+=end
+
+def add_one(n)
+  n += 1
+end
+
+a = 3
+add_one(a)
+a # => 3
+# Why is 'a' still 3? What is the return value of add_one?
+=begin
+
+=end
 
 
 # #EACH, #MAP, #SELECT WITH ARRAYS
@@ -68,9 +130,35 @@ end
 
 # VARIABLES AS POINTERS
 
+# Intro to Programming, More Stuff Chapter
+# Explain why in the first example b != a but in the second b == a
+# 1
+a = "hi there"
+b = a
+a = "not here"
+
+# 2
+a = "hi there"
+b = a
+a << ", Bob"
+=begin
+
+=end
 
 # PUTS VS RETURN
+a = [1, 2, 3]
 
+def mutate(array)
+  array.pop
+end
+
+p a
+p mutate(a)
+p a
+# Explain what each #p call will output and why
+=begin
+
+=end
 
 # FALSE VS NIL, TRUTHINESS
 
@@ -90,10 +178,74 @@ end
 3 == true # false; non-false non-nil objects are not true, they are truthy; they evaluate to true in boolean contexts
 false.nil? # false; false is not nil, nil just evaluates to false
 
+
 # METHOD DEFINITION, METHOD INVOCATION
+
+# Lesson 2, Section 19, More Variable Scope
+# method definition is defining a method using the def keyword
+def greeting
+  puts "Hello"
+end
+
+# method invocation is calling a method
+greeting # Calling the greeting method outputs "Hello"
+
+# calling a method with a block does not mean the block will execute; blocks ARE A PART of the method invocation
+# Lesson 2, Section 19, More Variable Scope
+def greetings
+  yield
+  puts "Goodbye"
+end
+
+word = "Hello"
+
+greetings do
+  puts word
+end
+=begin
+
+=end
+# What would the above code output without the 'yield' keyword?
+
 
 
 # IMPLICIT RETURN VALUE OF METHODS AND BLOCKS
 
+# Intro to Programming Book, Methods Chapter
+# "Ruby methods ALWAYS return the evaluated result of the last line of the expression unless an explicit return comes before it."
+def add_three(number)
+  return number + 3
+  number + 4
+end
+
+returned_value = add_three(4)
+puts returned_value
+=begin
+
+=end
+# What would the result be if the return keyword were removed?
+
 
 # ARRAY#SORT
+
+# Lesson 5 Section 2, Sorting
+# Integers: What will the following #sort invocation return?
+[2, 5, 3, 4, 1].sort # => 
+
+# Strings: What will the following #sort invocation return?
+['c', 'a', 'e', 'b', 'd'].sort # => 
+
+# Strings: What will the following #sort invocation return?
+%w(a A).sort
+
+# Can you sort an array with objects of different types?
+['a', 1].sort # => 
+
+# How does the <=> operator work? What will each of the following expressions return?
+2 <=> 1 # => 
+1 <=> 2 # => 
+2 <=> 2 # => 
+'b' <=> 'a' # => 
+'a' <=> 'b' # => 
+'b' <=> 'b' # => 
+1 <=> 'a' # => 
